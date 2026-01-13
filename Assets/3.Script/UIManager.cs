6ded079d6739ff20ac266924b8913981e2e2db2a
1Q <bsc6260@gmail.com>
2026-01-13 16:56:44 +0900

    0113 listen server

---
+++ Assets/3.Script/UIManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenuUI;
+    [SerializeField] GameObject hostMenuUI;
+    [SerializeField] GameObject clientMenuUI;
+
+    public void PressHostBtn()
+    {
+        Debug.Log("호스트 버튼 누름");
+        mainMenuUI.SetActive(false);
+        hostMenuUI.SetActive(true);
+        clientMenuUI.SetActive(false);
+    }
+
+    public void PressClientBtn()
+    {
+        Debug.Log("클라이언트 버튼 누름");
+        mainMenuUI.SetActive(false);
+        hostMenuUI.SetActive(false);
+        clientMenuUI.SetActive(true);
+    }
+
+    public void PressCreateBtn()
+    {
+        Debug.Log("방 생성 버튼 누름");
+        hostMenuUI.SetActive(false);
+    }
+
+    public void PressJoinBtn()
+    {
+        Debug.Log("참여 버튼 누름");
+        clientMenuUI.SetActive(false);
+    }
+}
